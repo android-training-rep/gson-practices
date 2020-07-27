4f94bd8241929be9b621bfdaa832966fa018f44e
Liao Xueqing <xueqingliao@foxmail.com>
2020-07-27 15:24:01 +0800

    配置证书

--- app/src/main/java/com/example/okhttppractices/NetworkActivity.java
+++ app/src/main/java/com/example/okhttppractices/NetworkActivity.java
@@ -3,6 +3,7 @@ package com.example.okhttppractices;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
@@ -13,13 +14,16 @@ import io.reactivex.Observable;
 import io.reactivex.ObservableEmitter;
 import io.reactivex.ObservableOnSubscribe;
 import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
 public class NetworkActivity extends AppCompatActivity {
     public static final String myUrl = "https://twc-android-bootcamp.github.io/fake-data/data/default.json";
+    private static final String TAG = "Network Activity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,6 +73,7 @@ public class NetworkActivity extends AppCompatActivity {
                             .url(myUrl)
                             .build();
                     Response response = client.newCall(request).execute();
+                    Log.d(TAG, "response="+response.body().toString());
                     if (response.isSuccessful()) {
                         emitter.onNext(response.body().toString());
                         emitter.onComplete();
@@ -80,5 +85,9 @@ public class NetworkActivity extends AppCompatActivity {
                 }
             }
         });
+
+        observable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribeWith(observer);
     }
 }
